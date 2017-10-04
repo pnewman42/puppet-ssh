7f40fb7ae70444930907347fef251587e82cdc61
root <root@pnewman421.mylabserver.com>
2017-10-04 00:28:52 +0000

    after lecture

--- manifests/init.pp
+++ manifests/init.pp
@@ -42,7 +42,23 @@
 #
 # Copyright 2017 Your name here, unless otherwise noted.
 #
-class ssh {
+class ssh(
+#  Boolean $permit_root_login = $::ssh::params::permit_root_login,
+#  Integer $port              = $::ssh::params::port,
+  String $package_name,
+  String $service_name,
+  String $ensure,
+  String $service_ensure,
+  Boolean $service_enable,
+  Boolean $permit_root_login = false,
+  Integer $port              = 22,
+) {
+  class { '::ssh::service': }
+  class { '::ssh::config': }
+  class { '::ssh::install': } 
 
-
+  Class['::ssh::install']
+  -> Class['::ssh::config']
+  ~> Class['::ssh::service']
+  -> Class['ssh']
 }
